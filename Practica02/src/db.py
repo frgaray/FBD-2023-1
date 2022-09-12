908e5ec2b5113d1557bbbc22457f3ad24e4caa66
angelAlk <angelalk@ciencias.unam.mx>
2022-09-12 14:45:11 -0500

    Practica2 scaffolding

---
+++ Practica02/src/db.py
@@ -0,0 +1,22 @@
+#!/bin/env python
+
+import sys
+
+# Architecture:
+
+# Command list:
+# init
+# 
+
+def main():
+    "Starting point for the database"
+
+    # parse arguments
+    # check & load database
+    # execute command into database
+    # write changes into files
+    # finish
+    print(sys.argv)
+
+main()
+
