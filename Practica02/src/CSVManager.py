1f75d4adec48407414e15df9e8a2b320336c1379
ianPineda <ianpineda@ciencias.unam.mx>
2022-09-13 10:44:54 -0500

    Fix functionality of CSVManager

--- Practica02/src/CSVManager.py
+++ Practica02/src/CSVManager.py
@@ -1,11 +1,14 @@
 import csv
+from typing import List
+import os
 
 class CSVManager:
 
-    def __init__(self, file_name: str = '/home/ianpineda/projects/FBD-2023-1/test.csv'):
+    def __init__(self, file_name: str):
         self.file_name = file_name
         self.dict = {}
-        pass
+        if not os.path.exists(file_name):
+            raise FileNotFoundError('No existe el archivo')
 
     def read(self):
         with open(self.file_name, mode = 'r') as file:
@@ -15,9 +18,12 @@ class CSVManager:
     def write(self):
         with open(self.file_name, mode = 'w') as file:
             writer = csv.writer(file)
-            writer.writerows(list(self.dict))
-            
+            rows:List[List[str]] = []
+            for i,j in self.dict.items():
+                j.insert(0, i)
+                rows.append(j)
 
-xd = CSVManager()
-xd.read()
-xd.write()
+            writer.writerows(rows)
+
+    def __getitem__(self, key:str):
+        return self.dict[key]
--- Practica02/src/db.py
+++ Practica02/src/db.py
@@ -1,12 +1,20 @@
 #!/bin/env python
 
 import sys
+from CSVManager import CSVManager
 
 # Architecture:
 
 # Command list:
 # init
 # 
+
+#Testing, delete afterwards----------------------------------------------------
+prueba = CSVManager('./test.csv')
+prueba.read()
+print(prueba['1'])
+prueba.write()
+#------------------------------------------------------------------------------
 
 def main():
     "Starting point for the database"
