26f0d347fc4dc95fa0d54bbfabfddc29c727426e
DrJzzz <josue9499@gmail.com>
2022-09-14 17:46:28 -0500

    blabla

--- Practica02/src/Viveros.py
+++ Practica02/src/Viveros.py
@@ -1,3 +1,12 @@
+from CSVManager import CSVManager
+from typing import  List
+from datetime import date
+from random import randint
+
 class Viveros:
-    def __init__(self):
-        pass
+    def __init__(self, viveros:CSVManager, empleados:CSVManager, correos:CSVManager, telefonos:CSVManager, viveros_telefonos:CSVManager):
+        self.viveros = viveros
+        self.empleados = empleados
+        self.correos = correos
+        self.telefonos = telefonos
+        self.viveros_telefonos = viveros_telefonos
