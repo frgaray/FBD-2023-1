2cffb1ba7232f2e59138a3f434d49684cc9326f2
ianPineda01 <ianpineda@ciencias.unam.mx>
2022-10-31 22:30:25 -0600

    ADD restricciones de dominio to DDL.sql

--- Practica06/SQL/DDL.sql
+++ Practica06/SQL/DDL.sql
@@ -5,14 +5,15 @@ DROP SCHEMA IF EXISTS public CASCADE;
 CREATE SCHEMA public;
 
 CREATE TYPE asoleo_planta AS ENUM ('sol', 'sombra', 'resolana');
+CREATE TYPE metodo_pago AS ENUM ('efectivo', 'debito', 'credito', 'transferencia', 'otro');
 
 CREATE TABLE Planta (
 	NombrePlanta varchar(256),
 	Cuidado text,
-	Genero varchar(128),-- deberian de ser solo los especificado en el documento?{Haworthia, Gasteria, Astrophytum, Ariocarpus}
-	Precio integer,--decimal en centavos (debería ser money?)
+	Genero varchar(128),
+	Precio integer,--decimal en centavos
 	TipoAsoleo asoleo_planta,
-	TipoOrigen varchar(128),-- enum? {Africana, Cactus},
+	TipoOrigen varchar(128),
 	FechaGerminacion date
 );
 
@@ -56,7 +57,7 @@ CREATE TABLE VentaElectronica (
 	IdVentaElectronica integer,
 	IdCliente integer,
 	NumeroProductos integer,
-	MetodoPago varchar(128),--enum ?
+	MetodoPago metodo_pago,
 	NumeroSeguimiento integer,
 	FechaPedido date,
 	Estado varchar(256),
@@ -77,7 +78,7 @@ CREATE TABLE Empleado (
 	Calle varchar(256),
 	NumeroExterior varchar(16),
 	Rol varchar(128), --{gerente, cuidador de plantas, encargado de mostrar a cliente, cajero}
-	Salario integer --centavos
+	Salario integer --decimal en centavos
 );
 
 CREATE TABLE EmpleadoTelefono (
@@ -96,7 +97,7 @@ CREATE TABLE VentaFisica (
 	AyudarIdEmpleado integer,
 	CobrarIdEmpleado integer,
 	NumeroProductos integer,
-	MetodoPago varchar(128)-- enum?
+	MetodoPago metodo_pago
 );
 
 CREATE TABLE EntregarVentaFisica (
@@ -146,21 +147,61 @@ ALTER TABLE EstarEn ADD FOREIGN KEY (NombreVivero) REFERENCES Vivero(NombreViver
 ALTER TABLE EstarEn ADD FOREIGN KEY (NombrePlanta) REFERENCES Planta(NombrePlanta);
 
 -- restricciones de dominio
+ALTER TABLE Planta ALTER COLUMN Genero SET NOT NULL;
 ALTER TABLE Planta ALTER COLUMN Precio SET NOT NULL;
+ALTER TABLE Planta ALTER COLUMN TipoAsoleo SET NOT NULL;
+ALTER TABLE Planta ALTER COLUMN TipoOrigen SET NOT NULL;
+ALTER TABLE Planta ALTER COLUMN FechaGerminacion SET NOT NULL;
 ALTER TABLE Planta ADD Constraint dinero CHECK (Precio >= 0);
 
+ALTER TABLE Vivero ALTER COLUMN FechaApertura SET NOT NULL;
+ALTER TABLE Vivero ALTER COLUMN Estado SET NOT NULL;
+ALTER TABLE Vivero ALTER COLUMN CP SET NOT NULL;
+ALTER TABLE Vivero ALTER COLUMN Calle SET NOT NULL;
+ALTER TABLE Vivero ALTER COLUMN NumeroExterior SET NOT NULL;
+
+ALTER TABLE Cliente ALTER COLUMN FechaNacimiento SET NOT NULL;
+ALTER TABLE CLiente ALTER COLUMN Nombre SET NOT NULL;
+ALTER TABLE Cliente ALTER COLUMN ApellidoP SET NOT NULL;
+ALTER TABLE Cliente ALTER COLUMN ApellidoM SET NOT NULL;
+ALTER TABLE Cliente ALTER COLUMN Estado SET NOT NULL;
+ALTER TABLE Cliente ALTER COLUMN CP SET NOT NULL;
+ALTER TABLE Cliente ALTER COLUMN Calle SET NOT NULL;
+ALTER TABLE Cliente ALTER COLUMN NumeroExterior SET NOT NULL;
+
+ALTER TABLE VentaElectronica ALTER COLUMN NumeroProductos SET NOT NULL;
+ALTER TABLE VentaElectronica ALTER COLUMN MetodoPago SET NOT NULL;
+ALTER TABLE VentaElectronica ALTER COLUMN NumeroSeguimiento SET NOT NULL;
+ALTER TABLE VentaElectronica ALTER COLUMN FechaPedido SET NOT NULL;
+ALTER TABLE VentaElectronica ALTER COLUMN Estado SET NOT NULL;
+ALTER TABLE VentaElectronica ALTER COLUMN CP SET NOT NULL;
+ALTER TABLE VentaElectronica ALTER COLUMN Calle SET NOT NULL;
+ALTER TABLE VentaElectronica ALTER COLUMN NumeroExterior SET NOT NULL;
+
+ALTER TABLE Empleado ALTER COLUMN FechaNacimiento SET NOT NULL;
+ALTER TABLE Empleado ALTER COLUMN Nombre SET NOT NULL;
+ALTER TABLE Empleado ALTER COLUMN ApellidoP SET NOT NULL;
+ALTER TABLE Empleado ALTER COLUMN ApellidoM SET NOT NULL;
+ALTER TABLE Empleado ALTER COLUMN Estado SET NOT NULL;
+ALTER TABLE Empleado ALTER COLUMN CP SET NOT NULL;
+ALTER TABLE Empleado ALTER COLUMN Calle SET NOT NULL;
+ALTER TABLE Empleado ALTER COLUMN NumeroExterior SET NOT NULL;
+ALTER TABLE Empleado ALTER COLUMN Rol SET NOT NULL;
+ALTER TABLE Empleado ALTER COLUMN Salario SET NOT NULL;
+ALTER TABLE Empleado ADD Constraint dinero CHECK (Salario >= 0);
+
+ALTER TABLE VentaFisica ALTER COLUMN CobrarIdEmpleado SET NOT NULL;
+ALTER TABLE VentaFisica ALTER COLUMN NumeroProductos SET NOT NULL;
+ALTER TABLE VentaFisica ALTER COLUMN MetodoPago SET NOT NULL;
+
+ALTER TABLE EntregarVentaFisica ALTER COLUMN IdVentaFisica SET NOT NULL;
+ALTER TABLE EntregarVentaFisica ALTER COLUMN NombrePlanta;
+
+ALTER TABLE EntregarVentaElectronica ALTER COLUMN IdVentaFisica SET NOT NULL;
+ALTER TABLE EntregarVentaElectronica ALTER COLUMN NombrePlanta SET NOT NULL;
+
+ALTER TABLE EstarEn ALTER COLUMN NombreVivero SET NOT NULL;
+ALTER TABLE EstarEn ALTER COLUMN NombrePlanta SET NOT NULL;
+ALTER TABLE EstarEn ALTER COLUMN NumeroDePlantasVivero SET NOT NULL;
+
 --faltan restricciones para:
---Planta
---Vivero
---ViveroTelefono
---Cliente
---ClienteTelefono
---ClienteCorreoElectronico
---VentaElectronica
---VentaFisica
---Empleado
---EmpleadoTelefono
---EmpleadoCorreoElectronico
---EstarEn
---EntregarVentaFisica
---EntregarVentaElectronica
