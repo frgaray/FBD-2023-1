269840c347e31e280a135d55dc5773737d5ffb7b
nsgbanks@ciencias.unam.mx <nsgbanks@ciencias.unam.mx>
2022-11-07 19:05:17 -0600

    DDL.sql fixed

--- Practica07/SQL/DDL.sql
+++ Practica07/SQL/DDL.sql
@@ -8,113 +8,115 @@ CREATE TYPE asoleo_planta AS ENUM ('sol', 'sombra', 'resolana');
 CREATE TYPE metodo_pago AS ENUM ('efectivo', 'debito', 'credito', 'transferencia', 'otro');
 
 CREATE TABLE Planta (
-	NombrePlanta varchar(256),
-	Cuidado text,
-	Genero varchar(128),
-	Precio integer,--decimal en centavos
+	NombrePlanta VARCHAR(256),
+	Cuidado TEXT,
+	Genero VARCHAR(128),
+	Precio INTEGER,--decimal en centavos
 	TipoAsoleo asoleo_planta,
-	TipoOrigen varchar(128),
-	FechaGerminacion date
+	TipoOrigen VARCHAR(128),
+	FechaGerminacion DATE,
+	IdVentaFisica INTEGER,
+	IdVentaElectronica INTEGER
 );
 
 CREATE TABLE Vivero (
-	NombreVivero varchar(256),
-	FechaApertura date,
-	Estado varchar(256),
-	CP char(5),--son 5 números fijos, los representamos como cadenas para evitar confusión en operaciones
-	Calle varchar(256),
-	NumeroExterior varchar(16)--Hay números que llevan letra, como en duplexes
+	NombreVivero VARCHAR(256),
+	FechaApertura DATE,
+	Estado VARCHAR(256),
+	CP CHAR(5),--son 5 números fijos, los representamos como cadenas para evitar confusión en operaciones
+	Calle VARCHAR(256),
+	NumeroExterior VARCHAR(16)--Hay números que llevan letra, como en duplexes
 );
 
 CREATE TABLE ViveroTelefono (
-	NombreVivero varchar(256),
-	Telefono char(10)
+	NombreVivero VARCHAR(256),
+	Telefono CHAR(10)
 );
 
 CREATE TABLE Cliente (
-	IdCliente integer,
-	FechaNacimiento date,
-	Nombre varchar(128),
-	ApellidoP varchar(128),
-	ApellidoM varchar(128),
-	Estado varchar(256),
-	CP char(5),
-	Calle varchar(256),
-	NumeroExterior varchar(16)
+	IdCliente INTEGER,
+	FechaNacimiento DATE,
+	Nombre VARCHAR(128),
+	ApellidoP VARCHAR(128),
+	ApellidoM VARCHAR(128),
+	Estado VARCHAR(256),
+	CP CHAR(5),
+	Calle VARCHAR(256),
+	NumeroExterior VARCHAR(16)
 );
 
 CREATE TABLE ClienteTelefono (
-	IdCliente integer,
-	Telefono char(10)
+text	IdCliente INTEGER,
+	Telefono CHAR(10)
 );
 
 CREATE TABLE ClienteCorreoElectronico (
-	IdCliente integer,
-	CorreoElectronico varchar(128)
+	IdCliente INTEGER,
+	CorreoElectronico VARCHAR(128)
 );
 
 CREATE TABLE VentaElectronica (
-	IdVentaElectronica integer,
-	IdCliente integer,
-	NumeroProductos integer,
+	IdVentaElectronica INTEGER,
+	IdCliente INTEGER,
+	NumeroProductos INTEGER,
 	MetodoPago metodo_pago,
-	NumeroSeguimiento integer,
-	FechaPedido date,
-	Estado varchar(256),
-	CP char(5),
-	Calle varchar(256),
-	NumeroExterior varchar(16)
+	NumeroSeguimiento INTEGER,
+	FechaPedido DATE,
+	Estado VARCHAR(256),
+	CP CHAR(5),
+	Calle VARCHAR(256),
+	NumeroExterior VARCHAR(16)
 );
 
 CREATE TABLE Empleado (
-	IdEmpleado integer,
-	NombreVivero varchar(256),
-	FechaNacimiento date,
-	Nombre varchar(128),
-	ApellidoP varchar(128),
-	ApellidoM varchar(128),
-	Estado varchar(256),
-	CP char(5),
-	Calle varchar(256),
-	NumeroExterior varchar(16),
-	Rol varchar(128), --{gerente, cuidador de plantas, encargado de mostrar a cliente, cajero}
-	Salario integer --decimal en centavos
+	IdEmpleado INTEGER,
+	NombreVivero VARCHAR(256),
+	FechaNacimiento DATE,
+	Nombre VARCHAR(128),
+	ApellidoP VARCHAR(128),
+	ApellidoM VARCHAR(128),
+	Estado VARCHAR(256),
+	CP CHAR(5),
+	Calle VARCHAR(256),
+	NumeroExterior VARCHAR(16),
+	Rol VARCHAR(128), --{gerente, cuidador de plantas, encargado de mostrar a cliente, cajero}
+	Salario INTEGER --decimal en centavos
 );
 
 CREATE TABLE EmpleadoTelefono (
-	IdEmpleado integer,
-	Telefono char(10)
+	IdEmpleado INTEGER,
+	Telefono CHAR(10)
 );
 
 CREATE TABLE EmpleadoCorreoElectronico (
-	IdEmpleado integer,
-	CorreoElectronico varchar(128)
+	IdEmpleado INTEGER,
+	CorreoElectronico VARCHAR(128)
 );
 
 CREATE TABLE VentaFisica (
-	IdVentaFisica integer,
-	IdCliente integer,
-	AyudarIdEmpleado integer,
-	CobrarIdEmpleado integer,
-	NumeroProductos integer,
+	IdVentaFisica INTEGER,
+	IdCliente INTEGER,
+	AyudarIdEmpleado INTEGER,
+	CobrarIdEmpleado INTEGER,
+	NumeroProductos INTEGER,
 	MetodoPago metodo_pago
 );
 
 CREATE TABLE EntregarVentaFisica (
-	IdVentaFisica integer,
-	NombrePlanta varchar(256)
+	IdVentaFisica INTEGER,
+	NombrePlanta VARCHAR(256)
 );
 
 CREATE TABLE EntregarVentaElectronica (
-	IdVentaElectronica integer,
-	NombrePlanta varchar(256)
+	IdVentaElectronica INTEGER,
+	NombrePlanta VARCHAR(256)
 );
 
 --Se llamaba En
 CREATE TABLE EstarEn (
-	NombreVivero varchar(256),
-	NombrePlanta varchar(256),
-	NumeroDePlantasVivero integer
+	NombreVivero VARCHAR(256),
+	NombrePlanta VARCHAR(256),
+	NumeroDePlantasVivero INTEGER
 );
 
 --- Restricciones:
@@ -132,7 +134,13 @@ ALTER TABLE EmpleadoCorreoElectronico ADD PRIMARY KEY (IdEmpleado, CorreoElectro
 ALTER TABLE VentaFisica ADD PRIMARY KEY (IdVentaFisica);
 
 --FK
+ALTER TABLE Planta ADD FOREIGN KEY (IdVentaFisica) REFERENCES VentaFisica(IdVentaFisica)
+ALTER TABLE Planta ADD FOREIGN KEY (IdVentaElectronica) REFERENCES VentaElectronica(IdVentaElectronica)
+
 ALTER TABLE Empleado ADD FOREIGN KEY (NombreVivero) REFERENCES Vivero(NombreVivero);
+
+ALTER TABLE EmpleadoTelefono ADD FOREIGN KEY (IdEmpleado) REFERENCES Empleado(IdEmpleado);
+
 ALTER TABLE VentaElectronica ADD FOREIGN KEY (IdCliente) REFERENCES Cliente(IdCliente);
 ALTER TABLE VentaFisica ADD FOREIGN KEY (IdCliente) REFERENCES Cliente(IdCliente);
 ALTER TABLE VentaFisica ADD FOREIGN KEY (AyudarIdEmpleado) REFERENCES Empleado(IdEmpleado);
@@ -147,6 +155,7 @@ ALTER TABLE EstarEn ADD FOREIGN KEY (NombreVivero) REFERENCES Vivero(NombreViver
 ALTER TABLE EstarEn ADD FOREIGN KEY (NombrePlanta) REFERENCES Planta(NombrePlanta);
 
 -- restricciones de dominio
+ALTER TABLE Planta ALTER COLUMN Cuidado SET NOT NULL;
 ALTER TABLE Planta ALTER COLUMN Genero SET NOT NULL;
 ALTER TABLE Planta ALTER COLUMN Precio SET NOT NULL;
 ALTER TABLE Planta ALTER COLUMN TipoAsoleo SET NOT NULL;
@@ -195,7 +204,7 @@ ALTER TABLE VentaFisica ALTER COLUMN NumeroProductos SET NOT NULL;
 ALTER TABLE VentaFisica ALTER COLUMN MetodoPago SET NOT NULL;
 
 ALTER TABLE EntregarVentaFisica ALTER COLUMN IdVentaFisica SET NOT NULL;
-ALTER TABLE EntregarVentaFisica ALTER COLUMN NombrePlanta;
+ALTER TABLE EntregarVentaFisica ALTER COLUMN NombrePlanta SET NOT NULL;
 
 ALTER TABLE EntregarVentaElectronica ALTER COLUMN IdVentaFisica SET NOT NULL;
 ALTER TABLE EntregarVentaElectronica ALTER COLUMN NombrePlanta SET NOT NULL;
