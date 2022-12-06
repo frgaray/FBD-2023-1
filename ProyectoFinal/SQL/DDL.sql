dd44a65834501c608669d98e4d2d02429567342d
garay <frga@ciencias.unam.mx>
2022-12-06 14:45:50 -0600

    better DDL

--- ProyectoFinal/SQL/DDL.sql
+++ ProyectoFinal/SQL/DDL.sql
@@ -3,145 +3,124 @@ CREATE SCHEMA public;
 
 CREATE TYPE DIA AS ENUM('Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo');
 
+CREATE TABLE Edificio (
+ IdEdificio INT,
+ Ubicacion varchar(255) NOT NULL,
+ PRIMARY KEY (IdEdificio)
+);
+
+
+CREATE TABLE Piso (
+ IdEdificio INT REFERENCES Edificio,
+ IdPiso INT,
+ Estatus varchar(255) NOT NULL,
+ PRIMARY KEY (IdEdificio, IdPiso)
+);
+
 CREATE TABLE Agente (
  CURP varchar(18),
- ApellidoMaterno varchar(255) SET NOT NULL,
- ApellidoPaterno varchar(255) SET NOT NULL,
- Nombre varchar(255) SET NOT NULL,
- FechaNacimiento INT SET NOT NULL,
- Correo varchar(255) SET NOT NULL,
- Fotografia varchar(255) SET NOT NULL,
+ ApellidoMaterno varchar(255) NOT NULL,
+ ApellidoPaterno varchar(255) NOT NULL,
+ Nombre varchar(255) NOT NULL,
+ FechaNacimiento INT NOT NULL,
+ Correo varchar(255) NOT NULL,
+ Fotografia varchar(255) NOT NULL,
  IdEdificio INT,
- IdPiso INT
+ IdPiso INT,
+ PRIMARY KEY (CURP),
+ FOREIGN KEY (IdEdificio, IdPiso) REFERENCES Piso ON UPDATE CASCADE ON DELETE CASCADE 
 );
 
 CREATE TABLE AgenteTelefono (
-  CURP varchar(255),
-  Telefono int
+ CURP varchar(255),
+ Telefono int,
+ PRIMARY KEY (CURP, Telefono)
 );
 
 
 CREATE TABLE AgenteHorario (
-  CURP varchar(18),
-  Dia varchar(255),
-  HoraEntrada INT,
-  HoraSalida INT
-);
-
-CREATE TABLE Edificio (
-  IdEdificio INT,
-  Ubicacion varchar(255) SET NOT NULL
-);
-
-CREATE TABLE Piso (
-  IdEdificio INT,
-  IdPiso INT,
-  Estatus varchar(255) SET NOT NULL
+ CURP varchar(18),
+ Dia varchar(255),
+ HoraEntrada INT,
+ HoraSalida INT,
+ PRIMARY KEY (CURP, Dia, HoraEntrada, HoraSalida)
 );
 
 CREATE TABLE Sala (
  IdSala INT,
- IdPiso INT SET NOT NULL
-);     	     
+ IdPiso INT,
+ IdEdificio INT,
+ PRIMARY KEY (IdSala),
+ FOREIGN KEY (IdPiso, IdEdificio) REFERENCES Piso ON UPDATE CASCADE ON DELETE CASCADE
+);     	     	
 
 CREATE TABLE Entrenador (
  CURP varchar(18),
- Domicilio varchar(255) SET NOT NULL,
- ApellidoPaterno varchar(255) SET NOT NULL,
- ApellidoMaterno varchar(255) SET NOT NULL,
- Nombre varchar(255) SET NOT NULL,
- FechaDeNacimiento DATE SET NOT NULL,
- Correo varchar(255) SET NOT NULL,
- Fotografia varchar(255) SET NOT NULL,
- NSS INT SET NOT NULL,
- FechaIngreso DATE SET NOT NULL,
- HorasPorSemana INT SET NOT NULL,
- IdSala INT
+ Domicilio varchar(255) NOT NULL,
+ ApellidoPaterno varchar(255) NOT NULL,
+ ApellidoMaterno varchar(255) NOT NULL,
+ Nombre varchar(255) NOT NULL,
+ FechaDeNacimiento DATE NOT NULL,
+ Correo varchar(255) NOT NULL,
+ Fotografia varchar(255) NOT NULL,
+ NSS INT NOT NULL,
+ FechaIngreso DATE NOT NULL,
+ HorasPorSemana INT NOT NULL,
+ IdSala INT,
+ PRIMARY KEY (CURP),
+ FOREIGN KEY (IdSala) REFERENCES Sala ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE EntrenadorTelefono (
  CURP varchar(18),
  Telefono INT,
+ PRIMARY KEY (CURP, Telefono)
 );
 
 CREATE TABLE EntrenadorHorario (
  CURP varchar(18),
  Dia DIA,
  HoraEntrada INT,
- HoraSalida INT
+ HoraSalida INT,
+ PRIMARY KEY (CURP, Dia, HoraEntrada, HoraSalida)
 );
 
 CREATE TABLE Estacion (
  IdEstacion INT,
- Mouse varchar(255) SET NOT NULL,
- Teclado varchar(255) SET NOT NULL,
- HeadSet varchar(255) SET NOT NULL,
- IdSala INT
-);
-
-CREATE TABLE ReservacionDias (
- IdReservacion INT,
- Dia DIA
-);
-
-CREATE TABLE Reservacion (
- IdReservacion INT,
- Duracion INT SET NOT NULL,
- Turno INT SET NOT NULL,
- SistemaOperativo INT SET NOT NULL,
+ Mouse varchar(255) NOT NULL,
+ Teclado varchar(255) NOT NULL,
+ HeadSet varchar(255) NOT NULL,
  IdSala INT,
- IdCliente INT 
+ PRIMARY KEY (IdEstacion),
+ FOREIGN KEY (IdSala) REFERENCES Sala ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE Cliente (
  RFC varchar(255),
- RazonSocial INT SET NOT NULL,
- Correo varchar(255) SET NOT NULL,
- ContactoApellidoPaterno varchar(255) SET NOT NULL,
- ContactoApellidoMaterno varchar(255) SET NOT NULL,
- ContactoNombre varchar(255) SET NOT NULL
+ RazonSocial INT NOT NULL,
+ Correo varchar(255) NOT NULL,
+ ContactoApellidoPaterno varchar(255) NOT NULL,
+ ContactoApellidoMaterno varchar(255) NOT NULL,
+ ContactoNombre varchar(255) NOT NULL,
+ PRIMARY KEY (RFC)
+);
+
+CREATE TABLE Reservacion (
+ IdReservacion INT,
+ Duracion INT NOT NULL,
+ Turno INT NOT NULL,
+ SistemaOperativo INT NOT NULL,
+ IdSala INT,
+ IdCliente varchar(255),
+ PRIMARY KEY (IdReservacion),
+ FOREIGN KEY (IdSala) REFERENCES Sala ON UPDATE CASCADE ON DELETE CASCADE,
+ FOREIGN KEY (IdCliente) REFERENCES Cliente ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE ReservacionDias (
+ IdReservacion INT REFERENCES Reservacion,
+ Dia DIA,
+ PRIMARY KEY (IdReservacion, Dia)
 );
 
 --Constraints:
-ALTER TABLE Agente ADD PRIMARY KEY (CURP);
-ALTER TABLE Agente ADD FOREIGN KEY (IdEdificio);
-ALTER TABLE Agente ADD FOREIGN KEY (IdPiso);
-ALTER TABLE Agente ADD FOREIGN KEY (IdEdificio) REFERENCES Piso(IdEdificio) ON UPDATE CASCADE ON DELETE CASCADE;
-ALTER TABLE Agente ADD FOREIGN KEY (IdPiso) REFERENCES Piso(IdPiso) ON UPDATE CASCADE ON DELETE CASCADE;
-
-ALTER TABLE AgenteTelefono ADD PRIMARY KEY (CURP);
-ALTER TABLE AgenteTelefono ADD PRIMARY KEY (Telefono);
-
-ALTER TABLE AgenteHorario ADD PRIMARY KEY (CURP);
-ALTER TABLE AgenteHorario ADD PRIMARY KEY (Dia);
-ALTER TABLE AgenteHorario ADD PRIMARY KEY (HoraEntrada);
-ALTER TABLE AgenteHorario ADD PRIMARY KEY (HoraSalida);
-
-ALTER TABLE Edifcio ADD PRIMARY KEY (IdEdificio);
-
-ALTER TABLE Piso ADD PRIMARY KEY (IdPiso);
-ALTER TABLE Piso ADD PRIMARY KEY (IdEdificio);
-
-ALTER TABLE Sala ADD PRIMARY KEY (IdSala);
-ALTER TABLE Sala ADD FOREIGN KEY (IdPiso) REFERENCES Piso(IdPiso) ON UPDATE CASCADE ON DELETE CASCADE;
-
-ALTER TABLE Entrenador ADD PRIMARY KEY (CURP);
-ALTER TABLE Entrenador ADD FOREIGN KEY (IdSala) REFERENCES Sala(IdSala) ON UPDATE CASCADE ON DELETE CASCADE;
-
-ALTER TABLE EntrenadorTelefono ADD PRIMARY KEY (CURP);
-ALTER TABLE EntrenadorTelefono ADD PRIMARY KEY (Telefono);
-
-ALTER TABLE EntrenadorHorario ADD PRIMARY KEY (CURP);
-ALTER TABLE EntrenadorHorario ADD PRIMARY KEY (Dia);
-ALTER TABLE EntrenadorHorario ADD PRIMARY KEY (HoraEntrada);
-ALTER TABLE EntrenadorHorario ADD PRIMARY KEY (HoraSalida);
-
-ALTER TABLE Estacion ADD PRIMARY KEY (IdEstacion);
-ALTER TABLE Estacion ADD FOREIGN KEY (IdSala) REFERENCES Sala(IdSala) ON UPDATE CASCADE ON DELETE CASCADE;
-
-ALTER TABLE Reservacion ADD PRIMARY KEY (IdReservacion);
-ALTER TABLE Reservacion ADD FOREIGN KEY (IdSala) REFERENCES Sala(IdSala) ON UPDATE CASCADE ON DELETE CASCADE;
-ALTER TABLE Reservacion ADD FOREIGN KEY (IdCliente) REFERENCES Cliente(RFC) ON UPDATE CASCADE ON DELETE CASCADE;
-
-ALTER TABLE ClienteTelefono ADD PRIMARY KEY (RFC);
-ALTER TABLE ClienteTelefono ADD PRIMARY KEY (Telefono);
