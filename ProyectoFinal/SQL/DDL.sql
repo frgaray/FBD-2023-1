1918e6dbe4fed5b1b5eef5fadea3f966bb041b55
garay <frga@ciencias.unam.mx>
2022-12-07 12:12:13 -0600

    DDL version 1.0

--- ProyectoFinal/SQL/DDL.sql
+++ ProyectoFinal/SQL/DDL.sql
@@ -3,7 +3,9 @@ CREATE SCHEMA public;
 
 CREATE TYPE DIA AS ENUM('Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo');
 CREATE TYPE ESTATUS AS ENUM('Disponible', 'Ocupado');
+CREATE TYPE MODALIDAD AS ENUM('Virtual', 'Presencial');
 CREATE TYPE SO AS ENUM('Linux', 'Windows');
+CREATE TYPE TIPOSALA AS ENUM('Capacitacion', 'Operaciones');
 CREATE TYPE TURNO AS ENUM('Matutino', 'Vespertino');
 
 CREATE TABLE Edificio (
@@ -20,18 +22,62 @@ CREATE TABLE Piso (
  PRIMARY KEY (IdEdificio, IdPiso)
 );
 
+CREATE TABLE Cliente (
+ RFC varchar(13),
+ RazonSocial varchar(255) NOT NULL,
+ Correo varchar(255) NOT NULL,
+ ContactoApellidoPaterno varchar(255) NOT NULL,
+ ContactoApellidoMaterno varchar(255) NOT NULL,
+ ContactoNombre varchar(255) NOT NULL,
+ PRIMARY KEY (RFC)
+);
+
+CREATE TABLE Sala (
+ IdSala INT,
+ Tipo TIPOSALA,
+ IdPiso INT,
+ IdEdificio INT,
+ PRIMARY KEY (IdSala),
+ FOREIGN KEY (IdPiso, IdEdificio) REFERENCES Piso ON UPDATE CASCADE ON DELETE CASCADE
+);     	     	
+
+CREATE TABLE Reservacion (
+ IdReservacion INT,
+ Duracion TIME NOT NULL,
+ Turno TURNO NOT NULL,
+ SistemaOperativo SO NOT NULL,
+ IdSala INT,
+ IdCliente varchar(13),
+ PRIMARY KEY (IdReservacion),
+ FOREIGN KEY (IdSala) REFERENCES Sala ON UPDATE CASCADE ON DELETE CASCADE,
+ FOREIGN KEY (IdCliente) REFERENCES Cliente ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE Curso (
+ IdCurso INT,
+ Modalidad MODALIDAD NOT NULL,
+ Turno TURNO NOT NULL,
+ Nombre varchar(255) NOT NULL,
+ IdReservacion INT,
+ PRIMARY KEY (IdCurso),
+ FOREIGN KEY (IdReservacion) REFERENCES Reservacion ON UPDATE CASCADE ON DELETE CASCADE 
+);
+
 CREATE TABLE Agente (
  CURP varchar(18),
  ApellidoMaterno varchar(255) NOT NULL,
  ApellidoPaterno varchar(255) NOT NULL,
  Nombre varchar(255) NOT NULL,
- FechaNacimiento INT NOT NULL,
+ FechaNacimiento DATE NOT NULL,
  Correo varchar(255) NOT NULL,
  Fotografia varchar(255) NOT NULL,
+ TomarCalificacion decimal,
  IdEdificio INT,
  IdPiso INT,
+ IdCurso INT,
  PRIMARY KEY (CURP),
- FOREIGN KEY (IdEdificio, IdPiso) REFERENCES Piso ON UPDATE CASCADE ON DELETE CASCADE 
+ FOREIGN KEY (IdEdificio, IdPiso) REFERENCES Piso ON UPDATE CASCADE ON DELETE CASCADE,
+ FOREIGN KEY (IdCurso) REFERENCES Curso ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE AgenteTelefono (
@@ -48,14 +94,6 @@ CREATE TABLE AgenteHorario (
  HoraSalida TIME,
  PRIMARY KEY (CURP, Dia, HoraEntrada, HoraSalida)
 );
-
-CREATE TABLE Sala (
- IdSala INT,
- IdPiso INT,
- IdEdificio INT,
- PRIMARY KEY (IdSala),
- FOREIGN KEY (IdPiso, IdEdificio) REFERENCES Piso ON UPDATE CASCADE ON DELETE CASCADE
-);     	     	
 
 CREATE TABLE Entrenador (
  CURP varchar(18),
@@ -98,32 +136,37 @@ CREATE TABLE Estacion (
  FOREIGN KEY (IdSala) REFERENCES Sala ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-CREATE TABLE Cliente (
- RFC varchar(13),
- RazonSocial INT NOT NULL,
- Correo varchar(255) NOT NULL,
- ContactoApellidoPaterno varchar(255) NOT NULL,
- ContactoApellidoMaterno varchar(255) NOT NULL,
- ContactoNombre varchar(255) NOT NULL,
- PRIMARY KEY (RFC)
-);
-
-CREATE TABLE Reservacion (
- IdReservacion INT,
- Duracion TIME NOT NULL,
- Turno TURNO NOT NULL,
- SistemaOperativo SO NOT NULL,
- IdSala INT,
- IdCliente varchar(13),
- PRIMARY KEY (IdReservacion),
- FOREIGN KEY (IdSala) REFERENCES Sala ON UPDATE CASCADE ON DELETE CASCADE,
- FOREIGN KEY (IdCliente) REFERENCES Cliente ON UPDATE CASCADE ON DELETE CASCADE
-);
-
 CREATE TABLE ReservacionDias (
  IdReservacion INT REFERENCES Reservacion,
  Dia DIA,
  PRIMARY KEY (IdReservacion, Dia)
 );
 
---Constraints:
+CREATE TABLE Ocupar (
+ IdReservacion INT REFERENCES Reservacion,
+ IdSala INT REFERENCES Sala,
+ PRIMARY KEY (IdReservacion, IdSala)
+);
+
+CREATE TABLE Impartir (
+ IdCurso INT REFERENCES Curso,
+ CURP varchar(18) REFERENCES Entrenador,
+ Dia DIA,
+ Horas TIME,
+ PRIMARY KEY (IdCurso, CURP, Dia, Horas)
+);
+
+CREATE TABLE HorasContratadasCurso (
+ IdCurso INT REFERENCES Curso,
+ Dia DIA,
+ Horas TIME,
+ PRIMARY KEY (IdCurso, Dia, Horas)
+);
+
+CREATE TABLE TomarHorasPorDia (
+ CURP varchar(18) REFERENCES Agente,
+ Dia DIA,
+ Horas TIME,
+ Asistencia boolean,
+ PRIMARY KEY (CURP, Dia, Horas)
+);
